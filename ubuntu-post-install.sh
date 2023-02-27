b276ce1887561c50f1a75739f5948197b15c49d8
Reinaldo G. P. Neto <reinaldo.neto@ufv.br>
2023-02-27 18:38:32 -0300

    Update ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -53,7 +53,8 @@
 #
 # Woeusb requirements & use:
 # - sudo apt install libxml2-dev libfuse-dev ntfs-3g-dev
-# - Download Wimlib (https://wimlib.net); extrair; ./configure; make; sudo make install;
+# - Download and compile Wimlib (https://wimlib.net); extrair; ./configure; make; sudo make install;
+# - sudo ldconfig -v
 # - git clone https://github.com/WoeUSB/WoeUSB
 # - ./woeusb --device path/Windows.iso /dev/sdX
 # ---------------------------- VARIÁVEIS --------------------------------- #
