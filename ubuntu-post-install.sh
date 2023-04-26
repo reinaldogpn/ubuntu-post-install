bd5ebf0b0008638c4a1a1dce710f7dee7aeb8f81
Reinaldo G. P. Neto <reinaldo.neto@ufv.br>
2023-04-26 14:07:25 -0300

    Update ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -29,12 +29,11 @@
 #     - Remoção de pacotes desnecessários e atualização geral do script.
 #   v3.2 31/10/2022, reinaldogpn:
 #     - (Re)Adição de alguns pacotes .deb e adição do flatpak Bottles em substituição ao Wine.
+#   v3.3 26/04/2023, reinaldogpn:
+#     - Reestruturação da função que instala pacotes .deb e inclusão de pacotes; adição de pacotes apt; remoção de alguns pacotes flatpak, adição de preferências de customização da dock do Ubuntu.
 #
 # ------------------------------------------------------------------------ #
 # Extras:
-# 
-# Fix for LoL "Critical Error":
-# - sudo sysctl -w abi.vsyscall32=0 && lutris
 #
 # Disable 2K Louncher on Steam's Civilization VI init options:
 # - eval $( echo "%command%" | sed "s/2KLauncher\/LauncherPatcher.exe'.*/Base\/Binaries\/Win64Steam\/CivilizationVI.exe'/" )
@@ -45,9 +44,6 @@
 # Bottles's permission to add programs shortcut to desktop:
 # - flatpak override com.usebottles.bottles --user --filesystem=xdg-data/applications
 #
-# Install Zotero .deb version:
-# - https://github.com/retorquere/zotero-deb#installing-zotero--juris-m
-#
 # VBox Extension Pack:
 # - https://download.virtualbox.org/virtualbox/7.0.2/Oracle_VM_VirtualBox_Extension_Pack-7.0.2.vbox-extpack
 #
@@ -57,17 +53,18 @@
 # - sudo ldconfig -v
 # - Download Woeusb (https://github.com/WoeUSB/WoeUSB/releases)
 # - sudo ./woeusb --device path/Windows.iso /dev/sdX
+#
 # ---------------------------- VARIÁVEIS --------------------------------- #
 
 # ***** PROGRAMAS *****
 PACOTES_APT=(
+  audacity
   calibre
   codeblocks
   dconf-editor
-  drawing
   flatpak
-  chrome-gnome-shell
   filezilla
+  gimp
   gnome-calendar
   gnome-extensions
   gnome-photos
@@ -76,36 +73,33 @@ PACOTES_APT=(
   gnome-sushi
   gnome-tweaks
   gnome-weather
+  inkscape
   nautilus-dropbox
   neofetch
   pinhole
   plocate
   qbittorrent
   rhythmbox
+  spotify
   virtualbox
   vlc
   zotero
 )
 
 PACOTES_DEB=(
-# Chrome
-  "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-# Discord
-  "https://dl.discordapp.net/apps/linux/0.0.25/discord-0.0.25.deb"
-# Visual Studio Code
-  "https://az764295.vo.msecnd.net/stable/3b889b090b5ad5793f524b5d1d39fda662b96a2a/code_1.69.2-1658162013_amd64.deb"
+  "https://discord.com/api/download?platform=linux&format=deb" # Discord
+  "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" # Visual Studio Code
+  "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" # Google Chrome
+  "https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb?_ga=2.220914843.2087402065.1682524928-1166232706.1682524928" # OnlyOffice
+  "https://updates.getmailspring.com/download?platform=linuxDeb" # Mailspring
 )
 
-DIRETORIO_DOWNLOAD_DEB="/home/$USER/Downloads/PACOTES_DEB"
+PACOTES_DEB_SIZE=${#PACOTES_DEB[@]}
 
 PACOTES_FLATPAK=(
-  com.spotify.Client                    # Spotify
-  com.usebottles.bottles                # Bottles
-  com.getmailspring.Mailspring          # Mailspring
-  io.github.mimbrero.WhatsAppDesktop    # Whatsapp
-  org.gtk.Gtk3theme.Yaru-dark           # Yaru-dark theme
-  org.gnome.Epiphany                    # Epiphany (Gnome Web)
-  org.onlyoffice.desktopeditors         # OnlyOffice
+  "io.github.mimbrero.WhatsAppDesktop"    # Whatsapp
+  "org.gtk.Gtk3theme.Yaru-dark"           # Yaru-dark theme
+  "org.gnome.Epiphany"                    # Epiphany (Gnome Web)
 )
 
 PACOTES_GAMES=(
@@ -124,24 +118,25 @@ VERMELHO='\e[1;91m'
 VERDE='\e[1;92m'
 SEM_COR='\e[0m'
 
-# ***** EXTRA *****
+# ***** PATHS *****
+DIRETORIO_DOWNLOAD_DEB="/home/$USER/Downloads/PACOTES_DEB"
 FILE="/home/$USER/.config/gtk-3.0/bookmarks"
 
 # Adicionar o diretório e o alias respectivamente
 DIRETORIOS=(
-/home/$USER/Projetos
+  "/home/$USER/Projetos"
 )
 
 ALIASES=(
-"/home/$USER/Projetos Projetos"
-"/home/$USER/Dropbox Dropbox"
+  "/home/$USER/Projetos Projetos"
+  "/home/$USER/Dropbox Dropbox"
 )
 
 # ------------------------------ FUNÇÕES --------------------------------- #
 realizar_testes()
 {
 # Internet conectando?
-if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
+if ! ping -c 1 8.8.8.8 -q ; then
   echo -e "${VERMELHO}[ERROR] - Seu computador não tem conexão com a internet. Verifique os cabos e o modem.${SEM_COR}"
   exit 1
 else
@@ -152,7 +147,7 @@ fi
 if [[ ! -x $(which wget) ]]; then
   echo -e "${VERMELHO}[ERROR] - O programa wget não está instalado.${SEM_COR}"
   echo -e "${AMARELO}[INFO] - Instalando wget ...${SEM_COR}"
-  sudo apt install wget -y &> /dev/null
+  sudo apt install wget -y 
 else
   echo -e "${VERDE}[INFO] - O programa wget já está instalado.${SEM_COR}"
 fi
@@ -161,23 +156,25 @@ fi
 remover_locks() 
 {
   echo -e "${AMARELO}[INFO] - Removendo locks...${SEM_COR}"
-  sudo rm /var/lib/dpkg/lock-frontend &> /dev/null
-  sudo rm /var/cache/apt/archives/lock &> /dev/null
+  sudo rm /var/lib/dpkg/lock-frontend 
+  sudo rm /var/cache/apt/archives/lock 
   echo -e "${VERDE}[INFO] - Locks removidos.${SEM_COR}"
 }
 
 adicionar_arquitetura_i386() 
 {
-  wget -qO- https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh
   echo -e "${AMARELO}[INFO] - Adicionando arquitetura i386...${SEM_COR}"
-  sudo dpkg --add-architecture i386 &> /dev/null
+  sudo dpkg --add-architecture i386 
 }
 
 atualizar_repositorios()
 {
   echo -e "${AMARELO}[INFO] - Atualizando repositórios ...${SEM_COR}"
-  curl -sL https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh | sudo bash &> /dev/null # Adds Zotero's repository
-  sudo apt update -y &> /dev/null
+  wget -qO- https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh
+  curl -sL https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh | sudo bash  # Adds Zotero's deb repository
+  curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg # Adds Spotify's deb repository
+  echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+  sudo apt update -y 
 }
 
 instalar_pacotes_apt()
@@ -186,7 +183,7 @@ instalar_pacotes_apt()
   for pacote in ${PACOTES_APT[@]}; do
     if ! dpkg -l | grep -q $pacote; then
       echo -e "${AMARELO}[INFO] - Instalando o pacote $pacote ...${SEM_COR}"
-      sudo apt install $pacote -y &> /dev/null
+      sudo apt install $pacote -y 
       if dpkg -l | grep -q $pacote; then
         echo -e "${VERDE}[INFO] - O pacote $pacote foi instalado.${SEM_COR}"
       else
@@ -200,19 +197,23 @@ instalar_pacotes_apt()
 
 instalar_pacotes_deb()
 {
+  # Download dos pacotes
   echo -e "${AMARELO}[INFO] - Baixando pacotes .deb ...${SEM_COR}"
-  for url in ${PACOTES_DEB[@]}; do
-    wget -c "$url" -P "$DIRETORIO_DOWNLOAD_DEB" &> /dev/null
+  for (( i = 0; i < PACOTES_DEB_SIZE; i++ )); do
+    url=${PACOTES_DEB[i]}
+    wget -O "/home/$USER/Downloads/package$i.deb" $url
   done
+  # ----------------------
+  # Instalação dos pacotes
   echo -e "${AMARELO}[INFO] - Instalando pacotes .deb baixados ...${SEM_COR}"
-  sudo dpkg -i $DIRETORIO_DOWNLOAD_DEB/*.deb &> /dev/null
-  sudo apt --fix-broken install -y &> /dev/null
+  sudo dpkg -i $DIRETORIO_DOWNLOAD_DEB/*.deb 
+  sudo apt --fix-broken install -y 
 }
 
 instalar_dependencias_allegro()
 {
   echo -e "${AMARELO}[INFO] - Instalando dependências do Allegro ...${SEM_COR}"
-  sudo apt install -y liballegro5-dev cmake g++ freeglut3-dev libxcursor-dev libpng-dev libjpeg-dev libfreetype6-dev libgtk2.0-dev libasound2-dev libpulse-dev libopenal-dev libflac-dev libdumb1-dev libvorbis-dev libphysfs-dev &> /dev/null
+  sudo apt install -y liballegro5-dev cmake g++ freeglut3-dev libxcursor-dev libpng-dev libjpeg-dev libfreetype6-dev libgtk2.0-dev libasound2-dev libpulse-dev libopenal-dev libflac-dev libdumb1-dev libvorbis-dev libphysfs-dev 
 }
 
 adicionar_repositorios_flatpak()
@@ -228,7 +229,7 @@ instalar_pacotes_flatpak()
   for pacote in ${PACOTES_FLATPAK[@]}; do
     if ! flatpak list | grep -q $pacote; then
       echo -e "${AMARELO}[INFO] - Instalando o pacote $pacote...${SEM_COR}"
-      sudo flatpak install -y flathub $pacote &> /dev/null
+      sudo flatpak install -y flathub $pacote 
       if flatpak list | grep -q $pacote; then
         echo -e "${VERDE}[INFO] - O pacote $pacote foi instalado.${SEM_COR}"
       else
@@ -244,12 +245,12 @@ instalar_driver_TPLinkT2UPlus()
 {
 #  (Instalação opcional) Driver do adaptador wireless TPLink Archer T2U Plus
   echo -e "${AMARELO}[INFO] - Instalando driver wi-fi TPLink...${SEM_COR}"
-  sudo apt install -y dkms git &> /dev/null
-  sudo apt install -y build-essential libelf-dev linux-headers-$(uname -r) &> /dev/null
+  sudo apt install -y dkms git 
+  sudo apt install -y build-essential libelf-dev linux-headers-$(uname -r) 
   mkdir $HOME/Downloads/rtl8812au/
-  git clone https://github.com/aircrack-ng/rtl8812au.git $HOME/Downloads/rtl8812au/ &> /dev/null
+  git clone https://github.com/aircrack-ng/rtl8812au.git $HOME/Downloads/rtl8812au/ 
   cd $HOME/Downloads/rtl8812au/
-  sudo make dkms_install &> /dev/null
+  sudo make dkms_install 
 #  se a instalação for abortada, executar o comando: "sudo dkms remove 8812au/5.6.4.2_35491.20191025 --all"
   echo -e "${VERDE}[INFO] - Driver wi-fi instalado!${SEM_COR}"
 }
@@ -260,7 +261,7 @@ instalar_suporte_games()
   for pacote in ${PACOTES_GAMES[@]}; do
     if ! dpkg -l | grep -q $pacote; then
       echo -e "${AMARELO}[INFO] - Instalando o pacote $pacote ...${SEM_COR}"
-      sudo apt install $pacote -y &> /dev/null
+      sudo apt install $pacote -y 
       if dpkg -l | grep -q $pacote; then
         echo -e "${VERDE}[INFO] - O pacote $pacote foi instalado.${SEM_COR}"
       else
@@ -272,6 +273,21 @@ instalar_suporte_games()
   done
 }
 
+instalar_lol_snap()
+{
+  echo -e "${AMARELO}[INFO] - Instalando League of Legends (snap)...${SEM_COR}"
+  sudo snap install --beta wine-platform-runtime
+  sudo snap install --beta wine-platform-5-staging
+  sudo snap install --beta wine-platform-7-staging-core20
+  sudo snap install --edge leagueoflegends --devmode
+  sudo snap refresh
+  if snap list | grep -q "leagueoflegends"; then
+    echo -e "${VERDE}[INFO] - O instalador de League of Legends está pronto, use 'snap run leagueoflegends' para concluir a instalação.${SEM_COR}"
+  else
+    echo -e "${VERMELHO}[ERROR] - Falha ao obter o instalador de League of Legends.${SEM_COR}"
+  fi
+}
+
 extra_config()
 {
 #  Cria pastas úteis e adiciona atalhos ao Nautilus
@@ -280,7 +296,7 @@ extra_config()
       echo -e "${VERDE}[INFO] - $FILE já existe.${SEM_COR}"
   else
       echo -e "${AMARELO}[INFO] - $FILE não existe. Criando...${SEM_COR}"
-      touch /home/$USER/.config/gkt-3.0/bookmarks &> /dev/null
+      touch /home/$USER/.config/gkt-3.0/bookmarks 
   fi
   for diretorio in ${DIRETORIOS[@]}; do
     mkdir $diretorio
@@ -288,18 +304,26 @@ extra_config()
   for _alias in "${ALIASES[@]}"; do
     echo file://$_alias >> $FILE
   done
+# Configurações para o dock do sistema
+  echo -e "${AMARELO}[INFO] - Aplicando as preferências à dock do sistema...${SEM_COR}"
   gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+  gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+  gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+  gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true
+# Instalando codecs extras
+  echo -e "${AMARELO}[INFO] - Instalando codecs adicionais...${SEM_COR}"
+  sudo apt install ubuntu-restricted-extras
 }
 
 upgrade_e_limpeza_sistema()
 {
   echo -e "${AMARELO}[INFO] - Fazendo upgrade e limpeza do sistema ...${SEM_COR}"
-  sudo apt dist-upgrade -y &> /dev/null
-  sudo flatpak update -y &> /dev/null
-  sudo snap refresh &> /dev/null
-  sudo apt autoclean &> /dev/null
-  sudo apt autoremove -y &> /dev/null
-  rm -rf $HOME/Downloads/rtl8812au $DIRETORIO_DOWNLOAD_DEB &> /dev/null
+  sudo apt dist-upgrade -y 
+  sudo flatpak update -y 
+  sudo snap refresh 
+  sudo apt autoclean 
+  sudo apt autoremove -y 
+  rm -rf $HOME/Downloads/rtl8812au $DIRETORIO_DOWNLOAD_DEB 
   neofetch
   echo -e "${VERDE}[INFO] - Configuração concluída!${SEM_COR}"
   echo -e "${AMARELO}[INFO] - Reinicialização necessária, deseja reiniciar agora? [S/n]:${SEM_COR}"
@@ -315,10 +339,11 @@ adicionar_arquitetura_i386
 atualizar_repositorios
 instalar_pacotes_apt
 instalar_pacotes_deb
-#instalar_dependencias_allegro
+instalar_dependencias_allegro
 adicionar_repositorios_flatpak
 instalar_pacotes_flatpak
 instalar_driver_TPLinkT2UPlus
-#instalar_suporte_games
+instalar_suporte_games
+instalar_lol_snap
 extra_config
 upgrade_e_limpeza_sistema
